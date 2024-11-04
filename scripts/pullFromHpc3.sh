e170902adea244ed9dd01f2f87ad3ac854e9973c
Zitong Li <zitongl5@uci.edu>
2024-11-04 10:34:00 -0800

    remove more files from tracking

--- scripts/pullFromHpc3.sh
+++
@@ -1,6 +0,0 @@
-cd /Users/zitongli/Documents/DTC-SpMM_ASPLOS24/
-# rsync -ahv --progress zitongl5@hpc3.rcic.uci.edu:/pub/zitongl5/FTC-MM/ ./
-# rsync -ahv --progress zitongl5@hpc3.rcic.uci.edu:/pub/zitongl5/FTC-MM/scripts/bench/ ./scripts/bench/
-# rsync -ahv --progress zitongl5@hpc3.rcic.uci.edu:/pub/zitongl5/FTC-MM/scripts/DTCSpMM/ ./scripts/DTCSpMM/
-rsync -ahv --progress zitongl5@hpc3.rcic.uci.edu:/pub/zitongl5/FTC-MM/scripts/graph_stat/ ./scripts/graph_stat/
-cd -
--- scripts/pushToHpc3.sh
+++
@@ -1,5 +0,0 @@
-cd /Users/zitongli/Documents/TCFMM/
-
-rsync -ahv --progress ./ zitongl5@hpc3.rcic.uci.edu:/pub/zitongl5/TCFMM
-
-cd -
