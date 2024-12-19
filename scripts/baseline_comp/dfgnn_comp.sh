f22a477d1d997e1247867b1f9cda9cac6f11e59b
zli96 <david.wrctg@gmail.com>
2024-12-18 23:42:15 -0800

    Enhance f3s kernel and testing scripts: - Updated kernel to include new sddmm function call in `f3s_kernel_only.py`. - tested the sddmm function. - Adjusted kernel invocation in `dfgnn_comp.sh` to include additional kernel names. - Refactored `TCFMM.cpp` to rename parameters for clarity and consistency. - Removed commented-out code in `TCFMM_kernel.cu` to clean up the implementation.  - commeted out spmm and softmax in fused3s for comparing against the new sddmm kernel

--- scripts/baseline_comp/dfgnn_comp.sh
+++ scripts/baseline_comp/dfgnn_comp.sh
@@ -1,3 +1,3 @@
 /pub/zitongl5/NCU-2024.3/ncu --set full -f --export dfgnn_f320_pubmed.ncu-rep --kernel-name "regex:f3s_m16n8k16_cuda_kernel|sddmmCooKernel|softMax_SPMM" python dfgnn_softmax_kernel_only.py
-/pub/zitongl5/NCU-2024.3/ncu --set full -f --import-source yes --source-folders /pub/zitongl5/TCFMM/src --export f3s_f320_pubmed.ncu-rep --kernel-name "regex:f3s_m16n8k16_cuda_kernel|sddmmCooKernel|softMax_SPMM" python f3s_kernel_only.py
+/pub/zitongl5/NCU-2024.3/ncu --set full -f --import-source yes --source-folders /pub/zitongl5/TCFMM/src --export f3s_f320_pubmed.ncu-rep --kernel-name "regex:f3s_m16n8k16_cuda_kernel|sddmm_kernel|sddmmCooKernel|softMax_SPMM" python f3s_kernel_only.py
 
